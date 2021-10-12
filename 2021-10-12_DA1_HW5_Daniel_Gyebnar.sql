45947218f306bde5ad8b05fe99ffa500130fc89a
DaniDataScience <daniel.gyebnar@gmail.com>
2021-10-12 17:00:06 +0200

    adding HW5

---
+++ 2021-10-12_DA1_HW5_Daniel_Gyebnar.sql
@@ -0,0 +1,56 @@
+{"bookstore":[
+   {
+   "Catgory":"COOKING",
+   "titles":[
+      {
+      "Language":"EN",
+      "Title":"Everyday Italian"
+      }],
+   "author":[
+      {
+      "Title":"Giada De Laurentiis"
+      }],
+   "year":"2005",
+   "price":"30.00",
+   "available":TRUE
+   },
+   {
+   "Catgory":"CHILDREN",
+   "titles":[
+      {
+      "Language":"EN",
+      "Title":"Harry Potter"
+      }],
+   "author":[
+      {
+      "Title":"J K Rowling"
+      }],
+   "year":"2005",
+   "price":"29.90",
+   "available":FALSE
+   },
+   {
+   "Catgory":"DATA",
+   "titles":[
+      {
+      "Language":"EN",
+      "Title":"The Data Warehouse Toolkit"
+      },
+      {
+      "Language":"HUN",
+      "Title":"Adattárház eszközök"
+      }],
+   "author":[
+      {
+      "Title":"Ralph Kimball"
+      },
+      {
+      "Title":"Mary Ross"
+      }],
+   "year":"2002",
+   "price":"87.00",
+   "available":TRUE
+   }
+
+]}
+
