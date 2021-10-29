f910fc99a2164e70825632bdc2cef1f041bf372e
DaniDataScience <65224169+DaniDataScience@users.noreply.github.com>
2021-10-29 19:57:38 +0200

    Add files via upload

--- Term_Project/2021-10-27_DA1_Term-Project-1.sql
+++ Term_Project/2021-10-27_DA1_Term-Project-1.sql
@@ -13,64 +13,72 @@ DROP TABLE IF EXISTS MedianHouseholdIncome;
 CREATE TABLE MedianHouseholdIncome
 	(State VARCHAR(8),
 	City VARCHAR(255),
-	Median_Income VARCHAR(8));
+	Median_Income DOUBLE);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/MedianHouseholdIncome2015.txt'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/MedianHouseholdIncome2015.csv'
 	INTO TABLE MedianHouseholdIncome
-	FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
+	FIELDS TERMINATED BY ';' ENCLOSED BY '"' ESCAPED BY '\\'
 	LINES TERMINATED BY "\r\n"
 	IGNORE 1 LINES
-	(State,City,Median_Income);
+	(State,City,@Median_Income)
+    SET Median_Income = NULLIF(@Median_Income,'');
 
 -- CREATING POVERTY RATE TABLE -----------------------------------
 DROP TABLE IF EXISTS PercentageBelowPoverty;
 CREATE TABLE PercentageBelowPoverty
 	(State VARCHAR(8),
 	City VARCHAR(250),
-	poverty_rate VARCHAR(8));
+	avg_poverty_rate DOUBLE);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PercentagePeopleBelowPovertyLevel.txt'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PercentagePeopleBelowPovertyLevel.csv'
 	INTO TABLE PercentageBelowPoverty
 	FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 	LINES TERMINATED BY '\r\n'
 	IGNORE 1 LINES
-	(State, City, poverty_rate);
+	(State, City, @avg_poverty_rate)
+    SET avg_poverty_rate = NULLIF(@avg_poverty_rate,'');
 
 -- CREATING SHARE RACE YB CITY TABLE --------------------------------
 DROP TABLE IF EXISTS ShareRaceByCity;
 CREATE TABlE ShareRaceByCity
 	(State VARCHAR(8),
 	City VARCHAR(255),
-	share_white VARCHAR(8),
-	share_black VARCHAR(8),
-	share_native_american VARCHAR(8),
-	share_asian VARCHAR(8),
-	share_hispanic VARCHAR(8));
+	share_white DOUBLE,
+	share_black DOUBLE,
+	share_native_american DOUBLE,
+	share_asian DOUBLE,
+	share_hispanic DOUBLE);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ShareRaceByCity.txt'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ShareRaceByCity.csv'
 	INTO TABLE ShareRaceByCity
 	FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
-	LINES TERMINATED BY "\n"
+	LINES TERMINATED BY "\r\n"
 	IGNORE 1 LINES
-	(State, City, share_white, share_black, share_native_american, share_asian, share_hispanic);
+	(State, City, @share_white, @share_black, @share_native_american, @share_asian, @share_hispanic)
+    SET share_white = NULLIF(@share_white,''),
+		share_black = NULLIF(@share_black,''),
+		share_native_american = NULLIF(@share_native_american,''),
+		share_asian = NULLIF(@share_asian,''),
+		share_hispanic = NULLIF(@share_hispanic,'');
 
 -- CREATE TABLE % OF PEOPLE OVER 25 COMPLETING HIGH SCHOOL ------------------
 DROP TABLE IF EXISTS PeopleOver25CompletingHighSchool;
 CREATE TABLE PeopleOver25CompletingHighSchool
 	(State varchar(8),
 	City varchar(255),
-	percent_completed_hs varchar(8));
+	percent_completed_hs DOUBLE);
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PercentOver25CompletedHighSchool.txt'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PercentOver25CompletedHighSchool.csv'
 	into table PeopleOver25CompletingHighSchool
 	FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
-	LINES TERMINATED BY "\n"
+	LINES TERMINATED BY "\r\n"
 	IGNORE 1 LINES
-	(State,City,percent_completed_hs);
+	(State,City,@percent_completed_hs)
+    SET percent_completed_hs = NULLIF(@percent_completed_hs,"-");
 
 -- CREATING POLICE SHOOTINGS TABLE -----------------------------------
 DROP TABLE IF EXISTS shootings2015;
-	CREATE TABLE shootings2015
+CREATE TABLE shootings2015
 	(id INTEGER NOT NULL,
 	name VARCHAR(255),
 	date DATE,
@@ -87,7 +95,7 @@ DROP TABLE IF EXISTS shootings2015;
 	body_camera VARCHAR(10)
 	);
 	
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PoliceKillingsUS.txt'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PoliceKillingsUS.csv'
 	INTO TABLE shootings2015
 	FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\'
 	LINES TERMINATED BY "\n"
@@ -99,8 +107,8 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PoliceKillingsUS
 -- -----------------------------------------------------------?
 
 -- Questions to be anwsered: 
--- - What is the distribution of shootings by race?
 -- - How many fatal shootings have happened in 2015 in the US?
+-- - What is the distribution of shootings by race?
 -- - How many shootings occured where the victim was black in places where black population is a minority?
 -- - What is the median income and poverty rate per race where shootings occured?
 
@@ -126,7 +134,6 @@ CREATE PROCEDURE Clean_medianhouseholdincome()
 	UPDATE medianhouseholdincome SET city_cleaned = TRIM(BOTH ' city' FROM city_cleaned);
 	UPDATE medianhouseholdincome SET city_cleaned = TRIM(BOTH ' town' FROM city_cleaned);
 	UPDATE medianhouseholdincome SET city_cleaned = TRIM(BOTH ' CDP' FROM city_cleaned);
-	UPDATE medianhouseholdincome SET Median_Income="" WHERE Median_Income="(X)";
 
 	ALTER TABLE medianhouseholdincome
 		ADD COLUMN id VARCHAR(255);
@@ -160,12 +167,7 @@ CREATE PROCEDURE Clean_shareracebycity()
 	UPDATE shareracebycity SET city_cleaned = TRIM(BOTH ' city' FROM city_cleaned);
 	UPDATE shareracebycity SET city_cleaned = TRIM(BOTH ' town' FROM city_cleaned);
 	UPDATE shareracebycity SET city_cleaned = TRIM(BOTH ' CDP' FROM city_cleaned);
-	UPDATE shareracebycity SET share_white="" WHERE share_white="(X)";
-	UPDATE shareracebycity SET share_black="" WHERE share_black="(X)";
-	UPDATE shareracebycity SET share_native_american="" WHERE share_native_american="(X)";
-	UPDATE shareracebycity SET share_asian="" WHERE share_asian="(X)";
-	UPDATE shareracebycity SET share_hispanic="" WHERE share_hispanic="(X)";
-	
+		
 	ALTER TABLE shareracebycity
 		ADD COLUMN id VARCHAR(255);
 	UPDATE shareracebycity SET id = concat(State,"-",city_cleaned);
@@ -203,7 +205,6 @@ CREATE PROCEDURE Clean_percentagebelowpoverty()
 	UPDATE percentagebelowpoverty SET city_cleaned = TRIM(BOTH ' city' FROM city_cleaned);
 	UPDATE percentagebelowpoverty SET city_cleaned = TRIM(BOTH ' town' FROM city_cleaned);
 	UPDATE percentagebelowpoverty SET city_cleaned = TRIM(BOTH ' CDP' FROM city_cleaned);
-	UPDATE percentagebelowpoverty SET poverty_rate="" WHERE poverty_rate="-";
 	
 	ALTER TABLE percentagebelowpoverty
 		ADD COLUMN id VARCHAR(255);
@@ -226,7 +227,7 @@ END //
 DELIMITER ;
 
 -- Over 25 completing high school -----------------------------------
-DROP PROCEDURE IF EXISTS peopleover25completinghighschool;
+DROP PROCEDURE IF EXISTS Clean_peopleover25completinghighschool;
 DELIMITER //
 CREATE PROCEDURE Clean_peopleover25completinghighschool()
 	BEGIN
@@ -237,7 +238,6 @@ CREATE PROCEDURE Clean_peopleover25completinghighschool()
 	UPDATE peopleover25completinghighschool SET city_cleaned = TRIM(BOTH ' city' FROM city_cleaned);
 	UPDATE peopleover25completinghighschool SET city_cleaned = TRIM(BOTH ' town' FROM city_cleaned);
 	UPDATE peopleover25completinghighschool SET city_cleaned = TRIM(BOTH ' CDP' FROM city_cleaned);
-	UPDATE peopleover25completinghighschool SET percent_completed_hs="" WHERE percent_completed_hs="-";
 	
 	ALTER TABLE peopleover25completinghighschool
 		ADD COLUMN id VARCHAR(255);
@@ -268,6 +268,29 @@ CALL Clean_peopleover25completinghighschool();
 CALL Clean_percentagebelowpoverty();
 CALL Clean_shareracebycity();
 
+-- ---------------------------------------------------------
+-- CREATING TRIGGER TO UPDATE TABLE -----------------------
+-- ----------------------------------------------------------
+
+DROP TRIGGER IF EXISTS update_shootings2015;
+CREATE TABLE IF NOT EXISTS messages (message varchar(100) NOT NULL); 
+DELIMITER //
+CREATE TRIGGER update_shootings2015
+AFTER INSERT
+ON shotings2015 FOR EACH ROW
+BEGIN
+	INSERT INTO messages SELECT CONCAT('new id: ', NEW.id);
+
+  	INSERT INTO shotings2015
+	SELECT *
+	FROM shotings2015
+	WHERE id = NEW.id
+	ORDER BY date, id;
+        
+END $$
+
+DELIMITER ;
+
 -- -----------------------------------------------------------
 -- CREATE DATA WAREHOUSE  ------------------------------------
 -- -----------------------------------------------------------
@@ -279,7 +302,7 @@ SELECT
     share_black, Black_pop_ratio,
 	percent_completed_hs,
 	Median_income,
-	poverty_rate
+	avg_poverty_rate
 FROM shootings2015
 LEFT JOIN shareracebycity
 ON shootings2015.city=shareracebycity.city_cleaned AND shootings2015.state=shareracebycity.State
@@ -308,14 +331,33 @@ SELECT count(*) FROM datawarehouse;
 -- What is the distribution of shootings by race
 DROP VIEW IF EXISTS Shooting_distribution_by_race;
 CREATE VIEW `Shooting_distribution_by_race` AS
-SELECT race, count(race) FROM datawarehouse GROUP BY race; 
+SELECT race, count(race), count(race)/2535 as Proportion FROM datawarehouse GROUP BY race; 
 
 -- How many shootings occured where the victim was black in places where black population is a minority
 DROP VIEW IF EXISTS Black_majority_or_minority;
 CREATE VIEW `Black_majority_or_minority` AS
-SELECT Black_pop_ratio,count(*) FROM datawarehouse WHERE race = "B" GROUP BY Black_pop_ratio;
+SELECT Black_pop_ratio,count(*),count(*)/618 as Proportion FROM datawarehouse WHERE race = "B" GROUP BY Black_pop_ratio;
 
--- What is the median income and poverty rate per race where shootings occured?
-DROP VIEW IF EXISTS Income_and_poverty_rate_per_race;
-CREATE VIEW `Income_and_poverty_rate_per_race` AS
-SELECT race, count(race),round(avg(median_income)), round(avg(poverty_rate)) FROM datawarehouse Group by race ;
+-- What is the poverty rate per race where shootings occured?
+DROP VIEW IF EXISTS Poverty_rate_per_race;
+CREATE VIEW `Poverty_rate_per_race` AS
+SELECT 
+	race, 
+    count(race),
+    round(avg(avg_poverty_rate)) as avg_poverty_rate,
+    round(min(avg_poverty_rate)) as min_poverty_rate,
+    round(max(avg_poverty_rate)) as max_poverty_rate,
+    round(std(avg_poverty_rate)) as std_poverty_rate
+FROM datawarehouse Group by race ;
+
+-- What is the median income per race where shootings occured?
+DROP VIEW IF EXISTS Median_income_per_race;
+CREATE VIEW `Median_income_per_race` AS
+SELECT 
+	race, 
+    count(race),
+    round(avg(Median_Income)) as Avg_Median_Income,
+    round(min(Median_Income)) as min_Median_Income,
+    round(max(Median_Income)) as max_Median_Income,
+    round(std(Median_Income)) as std_Median_Income
+FROM datawarehouse Group by race ;
